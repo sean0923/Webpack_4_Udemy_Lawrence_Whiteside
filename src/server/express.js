5e0cca4424ea0379e67b670293d0396d494ee5f6
Sean Hong <iamseanhong@gmail.com>
2018-03-29 20:13:41 -0700

    Add comment

--- src/server/express.js
+++ src/server/express.js
@@ -7,6 +7,7 @@ const webpack = require('webpack');
 const config = require('../../config/webpack.dev');
 const compiler = webpack(config);
 
+// Define Middlewares -------------------------------------------------------------
 const webpackDevMiddleware = require('webpack-dev-middleware')(
   compiler,
   config.devServer
@@ -15,12 +16,13 @@ const webpackDevMiddleware = require('webpack-dev-middleware')(
 const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
 
 const staticMiddleware = express.static('dist'); // or __dirname + '../../../dist'
+// --------------------------------------------------------------------------------
 
-
-// --- Order is important -----------
+// Use Middlewares ------------------
 app.use(webpackDevMiddleware); // 1
 app.use(webpackHotMiddleware); // 2
 app.use(staticMiddleware);     // 3
+// Order is important
 // ----------------------------------
 
 const PORT = 4000;
