bcc0ace9a70da1bb23c8fd40214eb64df1b85aa1
Sean Hong <iamseanhong@gmail.com>
2018-08-07 20:14:23 -0700

    Restart

--- src/main.js
+++
@@ -1,5 +0,0 @@
-
-require('webpack-hot-middleware/client?reload=true');
-require('./main.css');
-require('./index.html');
-require('./app.js');
--- src/server/main.js
+++
@@ -1,2 +0,0 @@
-require('babel-register'); // transpile everthing after by the rule of .babelrc
-require('./express'); // transpile express.js so that "import" can be used
